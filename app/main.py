80cc4f3d1f9562de9fe3839eb25c80dadc827c9f
Shankar <mailingmydad@icloud.com>
2026-07-27 18:06:39 +0000

    feat: add index page with htmx form

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from starlette.templating import _TemplateResponse
 
 from app.core import add
 from app.schemas import AddRequest, AddResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
 
 @app.post("/add", response_model=AddResponse)
 def read_root(request: AddRequest) -> AddResponse:
@@ -12,3 +15,7 @@ def read_root(request: AddRequest) -> AddResponse:
 @app.get("/health")
 def health_check() -> dict[str, str]:
     return {"status": "ok"}
+
+@app.get("/")
+def read_index(request: Request) -> _TemplateResponse:
+    return templates.TemplateResponse(request=request, name="index.html", context={})
